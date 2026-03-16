743a1d4c51e6b574a93734266de4d80192c28333
WinnieW685 <wwu82648@gmail.com>
2026-03-16 12:32:18 -0400

    yayme

---
+++ app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><h1>SHSAT by Schools</h1></div>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from 'vue' //watch used to run function
+import { useRoute } from 'vue-router'
+const schools = ref(null)
+async function getSchool(id) {
+  const response = await fetch(`https://data.cityofnewyork.us/resource/k8ah-28f4.json`)
+  const data = await response.json()
+  schools.value = data
+}
+</script>
+
+<style scoped></style>
