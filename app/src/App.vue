ada35b92f6c6231dc0ea6d4eb9d600833d559ed6
WinnieW685 <wwu82648@gmail.com>
2026-03-18 12:32:13 -0400

    yay

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <h1>SHSAT by Schools</h1>
+    <h1>SHSAT by Schools (2022)</h1>
     <h2>Search Schools by name</h2>
-    
+    <div class="container">
+      <SchoolCards v-for="school in schools" :key="school.feeder_school_dbn" :school="school">
+      </SchoolCards>
     </div>
-</div></div>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref, watch } from 'vue' //watch used to run function
+import SchoolCards from './components/SchoolCards.vue'
 import { useRoute } from 'vue-router'
 const schools = ref(null)
 async function getSchool() {
-  const response = await fetch(`https://data.cityofnewyork.us/resource/k8ah-28f4.json`)
+  const response = await fetch(
+    `https://data.cityofnewyork.us/resource/k8ah-28f4.json?$limit=683&$offset=800`,
+  )
   const data = await response.json()
   console.log(data)
   schools.value = data
@@ -21,4 +26,34 @@ getSchool()
 const route = useRoute()
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  padding: 16px;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+.container {
+  display: flex;
+  width: 80vw;
+  margin: 20px auto;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  border: 2px black;
+}
+.card {
+  background-color: rgb(227, 242, 255);
+  border: 1px solid #ccc;
+  padding: 12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
---
+++ app/src/components/SchoolCards.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ school.feeder_school_name }}</h1>
+    <h2>Test Takers: {{ school.count_of_testers }}</h2>
+    <h2>Accepted: {{ school.number_of_offers }}</h2>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  school: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+h1 {
+  font-size: medium;
+}
+h2 {
+  font-size: small;
+}
+div {
+  padding: 5px;
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background-color: rgb(218, 238, 255);
+  border-radius: 6px;
+}
+</style>
